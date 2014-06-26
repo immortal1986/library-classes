003d3b5b21c2b49a4c254df081a50d2df1e7fadd
immortal1986 <sysprag@gmail.com>
2014-06-26 09:57:09 +0300

    First try create factory method

--- index.php
+++ index.php
@@ -1,12 +1,40 @@
 <?php
-class A{
+interface CheakData{
+    function returnClearData($data);
+}
+class CheakIntegerData implements CheakData{
+    public $data;
+    function returnClearData($data){
+       if(is_numeric($data) && is_integer($data)){
+           return $this->data =  strip_tags(trim((integer)$data));
+       }elseif(is_float($data)){
+           return $this->data =  strip_tags(trim((float)$data));
+       }else{
+           return false;
+       }
+    }
+}
+class CheakStringData implements CheakData{
+    public $data;
+    function returnClearData($data){
+        if(is_string($data) && !empty($data)){
+            return $this->data =  strip_tags(trim((string)$data));
+        }else{
+            return false;
+        }
+    }
 
-    function doit()
-    {
-        echo __CLASS__;
+}
+class Factory{
+    public static function CreateCheakers($type){
+        switch($type){
+            case 'int':
+                 return new CheakIntegerData();
+            case 'str':
+                 return new CheakStringData();
+        }
+
     }
 
 
-
-    // Сашка сделай тут проверку данных!!
 }
