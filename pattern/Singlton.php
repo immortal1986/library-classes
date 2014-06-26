4cadcc3a368c846f2aeddd461689872c3e22b2e0
immortal1986 <sysprag@gmail.com>
2014-06-26 13:28:20 +0300

    Singlton pattern

---
+++ pattern/Singlton.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: S.koval
+ * Date: 26.06.14
+ * Time: 13:13
+ */
+
+namespace pattern;
+class Singlton{
+//  $_instance is our obj, default value is NULL
+    protected static $_instance = null;
+        public static function getInstance(){
+            if(self::$_instance instanceof self){
+                return self::$_instance;
+            }else{
+                return self::$_instance = new self;
+            }
+        }
+
+
+}
+
+
+
+//Create a two obj
+$obj = Singlton::getInstance();
+$objTwo = Singlton::getInstance();
+//Like see this is #1 obj!
+var_dump($obj);
+var_dump($objTwo);
